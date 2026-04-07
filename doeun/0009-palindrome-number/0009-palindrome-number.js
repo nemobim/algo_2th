a33eff5597fc00f6c07f943680bfa5d31206a769
Doeun <112849712+nemobim@users.noreply.github.com>
2026-04-07 23:17:45 +0900

    Time: 15 ms (12.11%), Space: 65.6 MB (8.91%) - LeetHub

--- doeun/0009-palindrome-number/0009-palindrome-number.js
+++ doeun/0009-palindrome-number/0009-palindrome-number.js
@@ -2,21 +2,32 @@
  * @param {number} x
  * @return {boolean}
  */
-const isPalindrome = function (x) {
-    // 양끝 부터 비교해서 다르면 false 처리
+// const isPalindrome = function (x) {
+//     // 양끝 부터 비교해서 다르면 false 처리
 
-    // 음수는 회문이 될 수 없음 (예: -121 -> 121-)
-    if (x < 0) return false;
+//     // 음수는 회문이 될 수 없음 (예: -121 -> 121-)
+//     if (x < 0) return false;
 
-    // 문자로 변환 후 양끝 비교
-    const s = String(x);
-    let left = 0;
-    let right = s.length - 1;
+//     // 문자로 변환 후 양끝 비교
+//     const s = String(x);
+//     let left = 0;
+//     let right = s.length - 1;
 
-    while (left < right) {
-        if (s[left] !== s[right]) return false;
-        left++;
-        right--;
-    }
-    return true;
+//     while (left < right) {
+//         if (s[left] !== s[right]) return false;
+//         left++;
+//         right--;
+//     }
+//     return true;
+// };
+
+const isPalindrome = function(x) {
+    // 1. 문자열로 바꾼다: "121"
+    // 2. 배열로 쪼갠다: ["1", "2", "1"]
+    // 3. 순서를 뒤집는다: ["1", "2", "1"]
+    // 4. 다시 합친다: "121"
+    const reversed = x.toString().split('').reverse().join('');
+    
+    // 원래 숫자와 뒤집은 문자가 같은지 비교
+    return x.toString() === reversed;
 };
