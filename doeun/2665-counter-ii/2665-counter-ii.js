8199e80e69f8f306525b5e56b977d56021f2d821
Doeun <112849712+nemobim@users.noreply.github.com>
2026-04-11 09:54:53 +0900

    💡 [LTC] UNKNOWN: 2665-counter-ii
    - Time: 58 ms (15.34%)
    - Space: 57.4 MB (31.53%)
    - Difficulty: Easy
    - Date: 04-11-2026

--- doeun/2665-counter-ii/2665-counter-ii.js
+++ doeun/2665-counter-ii/2665-counter-ii.js
@@ -1,22 +1,51 @@
+// /**
+//  * @param {integer} init
+//  * @return { increment: Function, decrement: Function, reset: Function }
+//  */
+// const createCounter = function (init) {
+//     //기존 값과 바뀌는 값을 들고 있어야된다.
+//     let origin = init
+//     let current = init
+
+//     const increment = () => current += 1
+//     const reset = () => current = origin
+//     const decrement = () => current -= 1
+
+//     return { increment, reset, decrement }
+// };
+
+// /**
+//  * const counter = createCounter(5)
+//  * counter.increment(); // 6
+//  * counter.reset(); // 5
+//  * counter.decrement(); // 4
+//  */
+
+// --------------- 보완 ----------------- / 
+
 /**
- * @param {integer} init
- * @return { increment: Function, decrement: Function, reset: Function }
+ * @param {number} init
+ * @return {{ increment: Function, decrement: Function, reset: Function }}
  */
 const createCounter = function (init) {
-    //기존 값과 바뀌는 값을 들고 있어야된다.
-    let origin = init
-    let current = init
+    // 1. 변수명은 의도가 명확하게 (initialValue)
+    const initialValue = init;
+    let currentCount = init;
 
-    const increment = () => current += 1
-    const reset = () => current = origin
-    const decrement = () => current -= 1
-
-    return { increment, reset, decrement }
+    // 2. 명시적인 return을 통해 예측 가능한 코드 작성
+    // 3. 단축 연산자(+=)를 활용하되, 가독성을 위해 화살표 함수 유지
+    return {
+        increment: () => {
+            currentCount += 1;
+            return currentCount;
+        },
+        decrement: () => {
+            currentCount -= 1;
+            return currentCount;
+        },
+        reset: () => {
+            currentCount = initialValue;
+            return currentCount;
+        }
+    };
 };
-
-/**
- * const counter = createCounter(5)
- * counter.increment(); // 6
- * counter.reset(); // 5
- * counter.decrement(); // 4
- */
